e3416089ff4236f56b02042d669690e88e65ea23
Alexis Belle <alexis.belle41@gmail.com>
2025-06-19 19:03:10 +0200

    Ajout de la nouvelle section oscar et des fleurs

--- template-parts/section-oscars.php
+++ template-parts/section-oscars.php
@@ -1,5 +1,14 @@
-
-  <div>
-        <h3>Fleurs d’oranger & chats errants est nommé aux Oscars Short Film Animated de 2022 !</h3>
-  </div>
-
+<section id="oscars" class="fade-in-up">
+      <div class="oscars-wrapper">
+            <div class="oscars-cloud">
+                  <h3 class="oscars-title">
+                  <span>Fleurs d’oranger & chats errants</span> 
+                  <span>est nommé aux Oscars Short</span> 
+                  <span>Film Animated de 2022 !</span>
+                  </h3>
+            </div>
+      <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/18-courts-metrages-francais-d-animation-eligibles-aux-oscars-2021.png" alt="Logo Oscars" />
+      </div>
+      <div class="flower flower--sunflower"></div>
+      <div class="flower flower--orchid"></div>
+</section>
